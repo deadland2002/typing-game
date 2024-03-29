2f52bee4688f22ae60712ab443738b7e21c9750a
satvik <satvikshukla453@gmail.com>
2024-03-29 15:15:41 +0530

    fixed css

--- frontend/pages/JoinGame.jsx
+++ frontend/pages/JoinGame.jsx
@@ -89,7 +89,7 @@ const TypeArea = ({questionNumber}) => {
 
 
   const [intervalState, setIntervalState] = useState();
-  const [timer, setTimer] = useState(60);
+    const [timer, setTimer] = useState(180);
   const [start, setStart] = useState(false);
 
   let ques = structuredClone(questions);
@@ -119,7 +119,32 @@ const TypeArea = ({questionNumber}) => {
   }, [timer]);
 
 
-
+  const HandleKeyDown = (event) => {
+    switch (event.key) {
+      case "ArrowUp":
+        console.log("Up arrow key pressed");
+        event.preventDefault(); // Prevent default action (like scrolling)
+        event.stopPropagation();
+        break;
+      case "ArrowDown":
+        console.log("Down arrow key pressed");
+        event.preventDefault(); // Prevent default action (like scrolling)
+        event.stopPropagation();
+        break;
+      case "ArrowLeft":
+        console.log("Left arrow key pressed");
+        event.preventDefault(); // Prevent default action (like scrolling)
+        event.stopPropagation();
+        break;
+      case "ArrowRight":
+        console.log("Right arrow key pressed");
+        event.preventDefault(); // Prevent default action (like scrolling)
+        event.stopPropagation();
+        break;
+      default:
+        break;
+    }
+  };
 
 
   const HandleSetQuestion = () => {
@@ -134,7 +159,7 @@ const TypeArea = ({questionNumber}) => {
 
     for (let i of arrayWords) {
       if (i.length >= 1) {
-        wordObj.push([i, "normal"]);
+        wordObj.push([i, "normal",undefined]);
       }
     }
 
@@ -144,6 +169,9 @@ const TypeArea = ({questionNumber}) => {
   };
 
   const HandleChange = async (e) => {
+
+    console.log(e.key)
+
     const { value } = e.target;
 
     setStarted(true);
@@ -152,12 +180,18 @@ const TypeArea = ({questionNumber}) => {
     }
 
     let words = value.split(" ");
+
+    console.log(words)
+
+    const ques = structuredClone(questions);
+
     if (
       words &&
       words.length >= 2 &&
       questions[words.length - 2][0] == words[words.length - 2]
     ) {
       ques[words.length - 2][1] = "correct";
+      ques[words.length - 2][2] = undefined;
       setQuestion(ques);
     } else if (
       words &&
@@ -170,10 +204,15 @@ const TypeArea = ({questionNumber}) => {
 
     if (words && words.length >= 1 && questions[words.length - 1]) {
       ques[words.length - 1][1] = "current";
-      if (ques[words.length - 2] && ques[words.length - 2][1] == "current") {
-        ques[words.length - 2][1] == "normal";
+      ques[words.length - 1][2] = words[words.length - 1];
+
+      if (ques[words.length] && ques[words.length][1] === "current") {
+        ques[words.length ][1] = "normal";
       }
+      console.log("called later",ques[words.length - 1])
+      setQuestion(ques)
     }
+
 
 
     HandlePingCorrect();
@@ -204,14 +243,31 @@ const TypeArea = ({questionNumber}) => {
         <label className="Question_Mount" htmlFor="input_text">
           {questions.map((single, index) => {
             return (
-              <span className={single[1]} key={`span_word_${index}`}>
-                {single[0]}
-              </span>
+                <div key={`span_word_${index}`}
+                     style={{
+                       position:"relative",
+                       margin:`10px 0px 5px 0px`
+                     }}>
+
+                  {
+                    single[2] ? <span className={single[1]}>{single[2]}</span> :
+                        <span className={single[1]}>{single[0]}</span>
+                  }
+
+                  {
+                      ["current"].includes(single[1]) && <span style={{
+                        position:"absolute",
+                        left:"0",
+                        top:"-25px"
+                      }} >{single[0]}</span>
+                  }
+
+                </div>
             );
           })}
         </label>
         <div className="Input_component">
-          <textarea id="input_text" type="text" onChange={HandleChange} />
+          <textarea id="input_text" type="text" onChange={HandleChange} onKeyDown={HandleKeyDown} />
         </div>
       </div>
     </>
