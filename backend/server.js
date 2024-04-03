b90524108323355101aba0d8f05ad3e47ab9978e
satvik <satvikshukla453@gmail.com>
2024-04-03 23:27:37 +0530

    added error handling

--- backend/server.js
+++ backend/server.js
@@ -87,115 +87,119 @@ function HandleCreateRoom(SocketId , RoomId , Name){
 }
 
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+try{
+  io.on("connection", (socket) => {
+    console.log("a user connected");
 
-  socket.on("join room",async (args)=>{
-    HandleJoinRoom(socket.id , args[0] , args[1])
-    socket.join("room_"+args[0])
+    socket.on("join room",async (args)=>{
+      HandleJoinRoom(socket.id , args[0] , args[1])
+      socket.join("room_"+args[0])
 
-    let player_list = structuredClone(rooms[args[0]].Players);
-    // delete player_list[socket.id]
-    
-    
-    io.to("room_"+args[0]).emit("player_list",player_list);
-    
-    console.log("rooms",JSON.stringify(rooms))
-  })
+      let player_list = structuredClone(rooms[args[0]].Players);
+      // delete player_list[socket.id]
 
 
+      io.to("room_"+args[0]).emit("player_list",player_list);
 
-  socket.on("room create",async (args)=>{
-    const {roomId,name} = args;
-    HandleCreateRoom(socket.id , roomId , name)
-    console.log("room-create",args)
-  })
+      console.log("rooms",JSON.stringify(rooms))
+    })
 
 
 
-  socket.on("get User",async ()=>{
-    const roomId = directory[socket.id].room;
-
-    console.log(rooms[directory[socket.id].room].Players)
-
-    const name = rooms[directory[socket.id].room].Players[socket.id].Name
-
-    socket.emit("receive user",name)
-
-    console.log("get User",roomId,name)
-  })
-
-  socket.on("disconnect", () => {
-    if(directory[socket.id] && rooms[directory[socket.id].room]){
-      delete rooms[directory[socket.id].room].Players[socket.id]
-    }
-
-    const roomId = directory[socket.id].room;
-
-    console.log("a user disconnected");
-    console.log(directory[socket.id]);
-    console.log(rooms[roomId].Players);
-
-    socket.leave("room_"+roomId)
-
-    let player_list = structuredClone(rooms[roomId].Players);
-
-    io.to("room_"+roomId).emit("player_list",player_list);
-
-    console.log(player_list)
-    
-    delete directory[socket.id]
-  });
+    socket.on("room create",async (args)=>{
+      const {roomId,name} = args;
+      HandleCreateRoom(socket.id , roomId , name)
+      console.log("room-create",args)
+    })
 
 
-  socket.on("Start Game",(args)=>{
-    console.log("pinged backend start")
-    console.log(directory[socket.id].room)
 
-    const roomId = directory[socket.id].room;
-    const AdminSocket = Object.keys(rooms[roomId].Admin)[0]
-    // if(socket.id === )
+    socket.on("get User",async ()=>{
+      const roomId = directory[socket.id].room;
 
-    console.log("Start Game",rooms[roomId])
-    if(socket.id !== AdminSocket){
-      return;
-    }
+      console.log(rooms[directory[socket.id].room].Players)
 
-    let max = 14;
-    let randomNumber;
-    const random = Math.random();
-    const scaledRandom = random * (max - 0 + 1);
-    randomNumber = Math.floor(scaledRandom) + 0;
+      const name = rooms[directory[socket.id].room].Players[socket.id].Name
 
-    io.to("room_"+directory[socket.id].room).emit("begin game",randomNumber)
-  })
-  
-  
-  
-  socket.on("Game Over",(args)=>{
-    console.log("pinged backend over")
-    let room_id = directory[socket.id].room;
-    let player_list = structuredClone(rooms[room_id].Players);
+      socket.emit("receive user",name)
+
+      console.log("get User",roomId,name)
+    })
+
+    socket.on("disconnect", () => {
+      console.log("a user disconnected");
+
+      if(directory[socket.id] && rooms[directory[socket.id].room]){
+        delete rooms[directory[socket.id].room].Players[socket.id]
+      }
+
+      if(directory[socket.id]){
+        const roomId = directory[socket.id].room;
+        socket.leave("room_"+roomId)
+        let player_list = structuredClone(rooms[roomId].Players);
+        io.to("room_"+roomId).emit("player_list",player_list);
+        console.log(player_list)
+        delete directory[socket.id]
+      }else{
+        console.log("socket not found in directory")
+      }
+    });
+
+
+    socket.on("Start Game",(args)=>{
+      if(!directory[socket.id])
+        return;
+
+      console.log("pinged backend start")
+      console.log(directory[socket.id].room)
+
+      const roomId = directory[socket.id].room;
+      const AdminSocket = Object.keys(rooms[roomId].Admin)[0]
+      // if(socket.id === )
+
+      console.log("Start Game",rooms[roomId])
+      if(socket.id !== AdminSocket){
+        return;
+      }
+
+      let max = 14;
+      let randomNumber;
+      const random = Math.random();
+      const scaledRandom = random * (max - 0 + 1);
+      randomNumber = Math.floor(scaledRandom) + 0;
+
+      io.to("room_"+directory[socket.id].room).emit("begin game",randomNumber)
+    })
+
+
+
+    socket.on("Game Over",(args)=>{
+      console.log("pinged backend over")
+      let room_id = directory[socket.id].room;
+      let player_list = structuredClone(rooms[room_id].Players);
       io.to("room_"+directory[socket.id].room).emit("game end",player_list)
-  })
-  
-  
-  
-  
-  socket.on("correct answers",(score)=>{
-    console.log("pinged backend score")
-    let room_id = directory[socket.id].room;
-    rooms[room_id].Players[socket.id].Points = score;
-    let player_list = structuredClone(rooms[room_id].Players);
-    let admin = structuredClone(rooms[room_id].Admin);
+    })
 
-    console.log("correct answers",player_list);
-    
-    io.to("room_"+room_id).emit("player_list",player_list);
-    io.to("room_"+room_id).emit("admin",admin);
 
-  })
-});
+
+
+    socket.on("correct answers",(score)=>{
+      console.log("pinged backend score")
+      let room_id = directory[socket.id].room;
+      rooms[room_id].Players[socket.id].Points = score;
+      let player_list = structuredClone(rooms[room_id].Players);
+      let admin = structuredClone(rooms[room_id].Admin);
+
+      console.log("correct answers",player_list);
+
+      io.to("room_"+room_id).emit("player_list",player_list);
+      io.to("room_"+room_id).emit("admin",admin);
+
+    })
+  });
+}catch (e){
+  console.log(JSON.stringify(e));
+}
 
 
 
